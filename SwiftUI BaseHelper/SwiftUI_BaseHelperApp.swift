d8342737690d6cfd4991cfbb12e5524e374a85c6
rodrigo.okido <rodrigo.okido@poatek.com>
2022-10-09 16:09:24 -0300

    Initial Commit

---
+++ SwiftUI BaseHelper/SwiftUI_BaseHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BaseHelperApp.swift
+//  SwiftUI BaseHelper
+//
+//  Created by Rodrigo Okido on 09/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BaseHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
